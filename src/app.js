b9f36058d7eeba2c2c7b6c0f00fe24971dffa5be
batmanba <berke.batman@cass.city.ac.uk>
2021-02-27 11:09:32 +0000

    third function exposed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const sayHello = require('./lib/strings');
 
 const app = express();
 
 app.get('/strings/hello/:id', (req, res) => {
   res.status(200).send({ result: `Hello, ${req.params.id}!` });
+});
+
+app.get('/strings/upper/:id', (req, res) => {
+  res.status(200).send({ result: req.params.id.toUpperCase() });
 });
 
 module.exports = app;
