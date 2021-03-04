22b835ad9c7b089a9941baf37697c58195996eb6
batmanba <berke.batman@cass.city.ac.uk>
2021-03-04 15:30:36 +0000

    numbers first function done

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 /* eslint-disable no-undef */
 const express = require('express');
 
+const { sayHello, uppercase, lowercase } = require('./lib/strings');
+
 const {
-  sayHello,
-  uppercase,
-  lowercase,
-  countCharacters,
-  firstCharacter,
-  firstCharacters,
-} = require('./lib/strings');
+  add,
+  subtract,
+  multiply,
+  divide,
+  power,
+  round,
+  roundUp,
+  roundDown,
+  absolute,
+  quotient,
+  remainder,
+} = require('./lib/numbers');
 
 const app = express();
 
@@ -28,6 +35,20 @@ app.get('/strings/lower/:id', (req, res) => {
 
 app.get('/strings/first-characters/:id?:a', (req, res) => {
   res.status(200).json({ result: req.params.id.substring(0, req.query.q) });
+});
+
+// numbers
+
+app.get('/numbers/add/:a/and/:b', (req, res) => {
+  // parameterised and assigned velaues as variables
+  const a = parseInt(req.params.a);
+  const b = parseInt(req.params.b);
+
+  if (isNaN(a) === false && isNaN(b) === false) {
+    res.status(200).json({ result: add(a, b) });
+  } else {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
 });
 
 module.exports = app;
