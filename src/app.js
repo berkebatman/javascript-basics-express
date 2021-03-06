5a246378c43a945f001d503d6b1bce6ac2af993a
batmanba <berke.batman@cass.city.ac.uk>
2021-03-06 18:38:24 +0000

    numberes progress

--- src/app.js
+++ src/app.js
@@ -19,6 +19,8 @@ const {
 
 const app = express();
 
+app.use(express.json());
+
 // strings
 
 app.get('/strings/hello/:id', (req, res) => {
@@ -50,5 +52,29 @@ app.get('/numbers/add/:a/and/:b', (req, res) => {
     res.status(400).json({ error: 'Parameters must be valid numbers.' });
   }
 });
+
+app.get('/numbers/subtract/:a/from/:b', (req, res) => {
+  // parameterised and assigned velaues as variables
+  const a = parseInt(req.params.a);
+  const b = parseInt(req.params.b);
+
+  if (isNaN(a) === false && isNaN(b) === false) {
+    res.status(200).json({ result: subtract(b, a) });
+  } else {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
+});
+
+app.post('/numbers/multiply', (req, res) => {
+  if (typeof req.body.a === 'undefined' || typeof req.body.b === 'undefined') {
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  } else if (isNaN(req.body.a) === false && isNaN(req.body.b) === false) {
+    res.status(200).json({ result: multiply(req.body.a, req.body.b) });
+  } else {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  }
+});
+
+// (typeof variable !== 'undefined')
 
 module.exports = app;
