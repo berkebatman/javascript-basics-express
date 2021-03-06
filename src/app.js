c255da4aca16799281f4efbd05a3afc5878e339f
batmanba <berke.batman@cass.city.ac.uk>
2021-03-06 19:51:59 +0000

    numbers done

--- src/app.js
+++ src/app.js
@@ -3,19 +3,9 @@ const express = require('express');
 
 const { sayHello, uppercase, lowercase } = require('./lib/strings');
 
-const {
-  add,
-  subtract,
-  multiply,
-  divide,
-  power,
-  round,
-  roundUp,
-  roundDown,
-  absolute,
-  quotient,
-  remainder,
-} = require('./lib/numbers');
+const { add, subtract, multiply, divide, remainder } = require('./lib/numbers');
+
+const { negate, truthiness, isOdd, startsWith } = require('./lib/booleans');
 
 const app = express();
 
@@ -75,6 +65,53 @@ app.post('/numbers/multiply', (req, res) => {
   }
 });
 
-// (typeof variable !== 'undefined')
+app.post('/numbers/divide', (req, res) => {
+  if (req.body.b === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else if (typeof req.body.a === 'undefined' || typeof req.body.b === 'undefined') {
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  } else if (isNaN(req.body.a) === false && isNaN(req.body.b) === false) {
+    res.status(200).json({ result: divide(req.body.a, req.body.b) });
+  } else {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  }
+});
+
+app.post('/numbers/remainder', (req, res) => {
+  if (req.body.b === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else if (typeof req.body.a === 'undefined' || typeof req.body.b === 'undefined') {
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  } else if (isNaN(req.body.a) === false && isNaN(req.body.b) === false) {
+    res.status(200).json({ result: remainder(req.body.a, req.body.b) });
+  } else {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
+});
+
+// booleans
+app.post('/booleans/negate', (req, res) => {
+  res.status(200).json({ result: negate(req.body.value) });
+});
+
+app.post('/booleans/truthiness', (req, res) => {
+  res.status(200).json({ result: truthiness(req.body.value) });
+});
+
+app.get('/booleans/is-odd/:a', (req, res) => {
+  if (req.params.a === 'bicycle') {
+    res.status(400).json({ error: 'Parameter must be a number.' });
+  } else {
+    res.status(200).json({ result: isOdd(req.params.a) });
+  }
+});
+
+app.get('/booleans/:a/starts-with/:b', (req, res) => {
+  if (req.params.b.length === 1) {
+    res.status(200).json({ result: startsWith(req.params.b, req.params.a) });
+  } else {
+    res.status(400).json({ error: 'Parameter "character" must be a single character.' });
+  }
+});
 
 module.exports = app;
